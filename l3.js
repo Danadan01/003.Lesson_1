097f2bcb62ff4626b783654029777ab3b2462b12
Danadan01 <danadan01zx@gmail.com>
2023-02-18 18:54:48 +0200

    Added template strings

---
+++ l3.js
@@ -0,0 +1,4 @@
+const userName = prompt("What is your name?", "Your name");
+const userAge = prompt("What is your age?", "Your age");
+const userFavColor = prompt("What is your favorite color?", "Your favorite color");
+alert(`Username is ${userName}, age is ${userAge} and favorite color is ${userFavColor}`);
